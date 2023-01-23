ab7d7430966ffe0f7326af6426f2df18a516c767
Fransnico Susanto <fransnico@amazeworx.com>
2023-01-23 23:26:55 +0700

    setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.php',
+    './**/*.php',
+    './resources/css/*.css',
+    './resources/js/*.js',
+    './safelist.txt',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
